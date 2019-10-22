1980b59fe1177c839978e65861176986b5ac1ec2
Lrenicus <bsastewart@gmail.com>
2019-10-21 19:52:22 -0600

    Completed lesson 22

---
+++ lessons/22-js-conditionals/index.js
@@ -0,0 +1,13 @@
+alert(22<12);
+
+let numOne = prompt('What is the first number?');
+alert(numOne);
+
+let numTwo = prompt('What is the second number?');
+alert(numTwo);
+
+if (numOne > numTwo) {
+    alert('It is greater!');
+} else if (numOne < numTwo) {
+    alert('It is less!');
+} else alert('They are is the same!')
